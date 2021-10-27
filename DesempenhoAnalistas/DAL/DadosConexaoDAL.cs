c48c88fe3cc00c231a53483062cfb1dd450b902d
Flávio Ferreira <flavinhoapf@hotmail.com>
2021-10-27 11:21:54 -0300

    Criadas as pastas DAL, GUI, BLL, DTO, UTIL, criado as classes DadosConexaoDAL, ConexaoDAL e ParametrosUTIL

---
+++ DesempenhoAnalistas/DAL/DadosConexaoDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DesempenhoAnalistas.UTIL.ParametrosUTIL;
+
+namespace DesempenhoAnalistas.DAL
+{
+    public class DadosConexaoDAL
+    {
+        public static String StringConexaoLocal
+        {
+            get
+            {
+                return @"Server="+ NpgSQLLocal.Servidor + "; Port=" + NpgSQLLocal.Porta + "; User Id=" + NpgSQLLocal.Usuario + "; Password=" + NpgSQLLocal.Senha + "; Database=" + NpgSQLLocal.Banco;
+            }
+        }
+    }
+}
---
+++ DesempenhoAnalistas/UTIL/ParametrosUTIL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesempenhoAnalistas.UTIL
+{
+    public class ParametrosUTIL
+    {
+        public static class NpgSQLLocal
+        {
+            public static string Servidor = "localhost";
+            public static string Porta = "5432";
+            public static string Banco = "CDA";
+            public static string Usuario = "postgres";
+            public static string Senha = "123456";
+        }
+    }
+}
